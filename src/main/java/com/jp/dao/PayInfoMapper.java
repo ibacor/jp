918dbb39583d7416c9edbd55cd6de79546feccba
bacor <cjp2012chen@163.com>
2019-07-02 15:06:07 +0800

    长期没有commit

---
+++ src/main/java/com/jp/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.jp.dao;
+
+import com.jp.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
---
+++ src/main/java/com/jp/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.jp.dao;
+
+import com.jp.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
